0f39e1a4ff53900694130a246af51f09440bae1d
timiajeojo <timi>
2026-01-09 05:37:29 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import SolanaLandingPage from '@/components/LandingPage';
+import LandingPage from '@/components/LandingPage';
 
 export default function Home() {
-  return <SolanaLandingPage />;
+  return <LandingPage />;
 }
