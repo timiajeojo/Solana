fec66235b4857a4ef8579cd899b1d3c5a5d46a23
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-25 00:56:43 +0100

    update app/nav component/AuthForm.tsx

--- app/nav component/AuthForm.tsx
+++ app/nav component/AuthForm.tsx
@@ -15,5 +15,7 @@ export default function AuthForm() {
   const [error, setError] = useState('');
   const router = useRouter()
   
-  const handleEmailAuth = async (e: React.);
+  const handleEmailAuth = async (e: React.FormEvent) => {
+    e.
+  };
 }
