e23b5c80bf3847275bae2c507c259aacead24d53
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-26 23:48:12 +0100

    update app/nav component/AuthForm.tsx

--- app/nav component/AuthForm.tsx
+++ app/nav component/AuthForm.tsx
@@ -55,8 +55,10 @@ export default function AuthForm() {
       </h2>
       
       {error && (
-       <div className=""
-        )
+       <div className="bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded-lg mb-4">
+       {error}
+       </div>
+        )}
     </div>
     
     )
