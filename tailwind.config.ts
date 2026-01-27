198d18c33d9a7db748dbe43849c9bbd3fad2ae26
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-27 02:33:26 +0100

    update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-};
+      colors: {
+        purple: {
+          50: "#faf5ff",
+          100: "#f3e8ff",
+          200: "#e9d5ff",
+          300: "#d8b4fe",
+          400: "#c084fc",
+          500: "#a855f7",
+          600: "#9333ea",
+          700: "#7e22ce",
+          800: "#6b21a8",
+          900: "#581c87"
+        }
+      }
+        }
+      }
+      plugins: [],
+}
 export default config;
