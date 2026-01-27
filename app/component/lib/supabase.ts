497bb3082042e6c33bdb9b0a3c5de1f071a4e44b
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-27 09:38:38 +0100

    update app/component/lib/supabase.ts

--- app/component/lib/supabase.ts
+++ app/component/lib/supabase.ts
@@ -1,3 +1,4 @@
 import { createClient  } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.;
+const supabaseUrl = process.env.https://exfvkdeewzlneuhldtfq.supabase.co;
+const supabaseAnonKey = process.env;
