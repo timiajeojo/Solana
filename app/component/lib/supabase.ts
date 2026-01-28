bc03137f6912a1fb664a058c6ea150f665d06642
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-28 14:21:35 +0100

    update app/component/lib/supabase.ts

--- app/component/lib/supabase.ts
+++ app/component/lib/supabase.ts
@@ -125,5 +125,6 @@ export async function signOut() {
   const { data, error } = await supabase.auth.signOut();
   if (error) {
     console.error('Error signing out:', error)
+    throw error
   }
 }
