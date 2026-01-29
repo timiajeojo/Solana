ae19b3cbdb73e56dc1f8b1cfee9a7c37aeaa265a
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-29 07:09:17 +0100

    update components/Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
-import React from 'react'
-import Link from 'next/link'
-import { usePathname, useRouter } from 'next/navigation'
-import { layoutDashboard, History, TrendingUp, LogOut } from 'lucide-react'
-import { signOut } from '../app/component/lib/supabase'
+import React from 'react';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
+import { layoutDashboard, History, TrendingUp, LogOut } from 'lucide-react';
+import { signOut } from '../app/component/lib/supabase';
 
 export 
