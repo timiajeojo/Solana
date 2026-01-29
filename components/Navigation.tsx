1944e775155c3de228ccc2f51413881af12aa611
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-29 07:47:36 +0100

    update components/Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -32,6 +32,7 @@ export default function Navigation() {
       <div className="w-10 h-10 bg-purple-600 rounded-lg flex items-center justify-center">
       <TrendingUp className="w-6 h-6 text-white" />
       </div>
-      <span className=""
+      <span className="text-xl font-bold text-black"> Solana Tracker </span>
+      
       )
 }
