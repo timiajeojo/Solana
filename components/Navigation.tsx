3a09634d950c7876b384ab2e23734d311239acd4
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-01-30 16:27:44 +0100

    update components/Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import { layoutDashboard, History, TrendingUp, LogOut } from 'lucide-react';
+import { LayoutDashboard, History, TrendingUp, LogOut } from 'lucide-react';
 import { signOut } from '../app/component/lib/supabase';
 
 export default function Navigation() {
