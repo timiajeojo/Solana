ee9a288dd889471467a8c0929493382aa2f6859b
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-02-03 07:42:36 +0100

    update app/auth/page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -18,64 +18,41 @@ export default function AuthPage() {
   const [isMenuOpen, setIsMenuOpen] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-}
-
-const  handleSubmit = async () => {
-  setError('')
-  setLoading(true)
-//validation
-if (!email || !password) {
-  setError('Please fill all required field')
-  setLoading(false)
-  return
-}
-
-if (!isSignIn && password.length < 6 ) {
-  setError('Password must be around 6 characters')
-  setLoading(false)
-  return
-}
-
-try {
-  if (!isSignIn) {
-    //signIn 
-    const result = await signInWithEmail(email, password);
-    console.log('successful:', result)
-    router.push('/dashboard')
-  } else {
-    //signup 
-    const result = await signUpWithEmail(email, password, firstName, lastName);
-    console.log('successful:', result)
-    alert('Success! check your email to verify yoir account before signing in')
-    //switch to signin tab
-    setIsSignIn(true)
-    setEmail('')
-    setPassword('')
-    setConfirmPassword('')
-    setFirstName('')
-    setLastName('')
-  }
-} catch (err) {
-  console.error('Auth error:', err);
-  setError(err.message || 'Authentication failed, please try again.')
-} } finally {
-  setLoading(false)
-}
-};
-
-const handleWalletSignIn = () => {
-  console.log('Wallet sign in not implemented yet')
-  setError('Wallet sign in coming soon')
-};
-
-const handleGoogleSignIn = async () => {
-  setError('')
-  setLoading(true)
   
-  try {
+  const handleSubmit = async () => {
+    setError('')
+    setLoading(true)
     
-  } catch (err) {
-    console.error('Error:', err);
+    //validation
+    if (!email || !password) {
+      setError('Please fill in all required fields')
+      setLoading(false)
+      return
+    }
     
-  }
-};
+    if (!isSignIn &&(!firstName || !lastName)) {
+      setError('Please enter your first and last name')
+      setLoading(false)
+      return
+    }
+    
+    if (!isSignIn && password !== confirmPassword) {
+      setError('Password do not match')
+      setLoading(false)
+      return
+    }
+    
+    if (!isSignIn && password.length < 6) {
+      setError('Password must be at least 6 characters')
+      setLoading(false)
+      return
+    }
+    
+    try {
+      
+    } catch (err) {
+      console.error('Error:', err);
+      
+    }
+  };
+}
