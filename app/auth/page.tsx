109e25a1a3b935d8363985b37870b266d404f53e
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-02-03 05:49:33 +0100

    update app/auth/page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,4 +2,10 @@
 
 import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { signUpWithEmail, signInWithEmail, signInWithGoogle }
+import { signUpWithEmail, signInWithEmail, signInWithGoogle } from '.../componenet/lib/supabase'
+
+export default function AuthPage() {
+  const router = useRouter();
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+}
