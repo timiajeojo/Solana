ef577d185f4a71d34577e62ad3a1e56401bc6e45
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-02-09 17:55:08 +0100

    update app/dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,5 +6,59 @@ import { TrendingUp, Wallet, ArrowUpRight, ArrowDownRight, Plus, User, Settings,
 import { getCurrentUser, getInvestments, addInvestment, getUserProfile, signOut } from '../component/lib/supabase'
 
 interface Investment {
+  Id?: number
+  amount: number
+  sol_price: number
+  sol_amount: number
+  purchase_date: string
+}
+
+export default function DashboardPage() {
+  const router = useRouter()
+  const [user, setUser] = useState<any>(null);
+  const [iserProfile, setUserProfile] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [investments, setInvestments] = useState<Investment[]>([]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [newInvestment, setNewInvestment] = useState({
+    solAmount:'',
+    pricePerSol: ''
+  })
+  const [currentSolPrice] = useState(102.30);
   
+  useEffect(() => {
+    checkUser()
+  }, [])
+  
+  const checkUser = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      if (!currentUser) {
+        router.push('/auth')
+        return
+      }
+      setUser(currentUser)
+      
+      const profile = await getUserProfile(currentUser.id);
+      setUserProfile(profile)
+      
+      await loadInvestments(currentUser.id)
+    } catch (error) {
+      console.error('Error checking user:', error);
+      router.push('/auth')
+    } finally {
+    setLoading(false)
+    }
+  };
+
+const loadInvestments = async (userId: string) => {
+  try {
+    const data = await getInvestments(userId);
+    setInvestments(data || [])
+  } catch (error) {
+    console.error('Error loading investments:', error);
+  }
+};
+
 }
