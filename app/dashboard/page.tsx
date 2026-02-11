91818cc7091392d6d040e589e0dcc230d0247ada
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-02-11 05:01:56 +0100

    update add investment modals

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -348,6 +348,10 @@ return (
     </div>
     </div>
     
-    
+    {/* Add Investment Modals */}
+    {showAddModal && (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+      
+      )}
     
 }
