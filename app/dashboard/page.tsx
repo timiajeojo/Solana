190f7cdf3e26ed743dadec7353f69d6b0c6e609d
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2026-02-11 03:36:14 +0100

    update app/dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -276,6 +276,7 @@ return (
     
     <div className="bg-white rounded-2xl p-6 shadow-lg border- border-purple-100">
     <div className="flex items-center gap-3 mb-4">
-    
+    <div className="w-12 h-12 rounded-xl bg-purple-100 flex items-center justify-center">
+    <TrendingUp className="w-6 h-6 bg-purple-100"
   )
 }
