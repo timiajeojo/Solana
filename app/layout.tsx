34949ba6b5955cefaa34d3dedb4df1ed29173fbf
timiajeojo <timiajeojo@gmail.com>
2026-06-04 11:10:32 +0100

    added user provide in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { UserProvider } from '@/context/UserContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -35,7 +36,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <UserProvider>
+      {children}
+      </UserProvider>
+      </body>
     </html>
   );
 }
