7a04e9f798f7dceeca86883a22ca3ae7cef9866f
timiajeojo <timiajeojo@gmail.com>
2026-06-06 19:02:56 +0100

    fix userContext import statment

--- app/context/UserContext.tsx
+++ app/context/UserContext.tsx
@@ -12,7 +12,7 @@ import {
   supabase,
   getUserProfile,
   updateUserProfile,
-} from "@/component/lib/supabase";
+} from "@/app/component/lib/supabase";
 
 // ─── Types ────────────────────────────────────────────────────────────────────
 
